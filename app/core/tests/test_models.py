4a8a6bbcdfa25ad6572ba793fefea625eb3c97cb
Uffe <uffe@ulflundgren.se>
2020-02-12 10:10:47 +0100

    Correct filename Completed user testcase

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -7,10 +7,10 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email"""
         email = 'test@ulflundgren.se'
-
         password = 'Testpass123'
         user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
-        # TODO: Fjuk!!!
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
